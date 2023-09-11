35333dd12288c9dffd9a64be16c735f8bb0ee73e
zvef <ancientelfhighlander@yahoo.com>
2023-09-10 20:34:51 -0400

    THIRD

---
+++ main.js
@@ -0,0 +1,10 @@
+//Main goal is to have as little global code as possible
+//Use factory functions and modules to create gameObjects
+
+//Use modules for the gameBoard and displayController
+//Use factories for players, or anything you need multiples of
+
+const gameBoardContainer = document.getElementById('gameBoardContainer');
+
+
+console.log(gameBoardContainer)
